0c6a14677558d2abe189d2ff2dbccf87f1325351
lewis wege <wegelewis7@gmail.com>
2024-07-08 18:57:51 +0300

    improved code

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ dragElement(document.getElementById('plant12'));
 dragElement(document.getElementById('plant13'));
 dragElement(document.getElementById('plant14'));
 
-
 function dragElement(terrariumElement) {
 	//set 4 positions for positioning on the screen
 	let pos1 = 0,
@@ -21,16 +20,27 @@ function dragElement(terrariumElement) {
 		pos3 = 0,
 		pos4 = 0;
 	terrariumElement.onpointerdown = pointerDrag;
+
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onpointermove = elementDrag;
-        document.onpointerup = stopDragElement;
+        document.onpointerup = stopElementDrag;
     }
 
-    function stopDragElement() {
+    function elementDrag(e) {
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        console.log(pos1, pos2, pos3, pos4);
+        terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+    }
+
+    function stopElementDrag() {
         document.onpointerup = null;
         document.onpointermove = null;
     }
